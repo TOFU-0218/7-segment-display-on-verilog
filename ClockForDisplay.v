ca2b5cedca393043bb36ed47b4da709f7976570f
TOFU-0218 <maikura5963@gmail.com>
2024-02-06 17:59:37 +0900

    ディスプレイ用の動作周波数の変更

--- ClockForDisplay.v
+++ ClockForDisplay.v
@@ -2,11 +2,11 @@ module ClockForDisplay(
     input i_clk,
     output reg o_clockForDisplay = 0
 );
-    reg [18:0] count = 19'b0;
+    reg [16:0] count = 17'b0;
 
     always @(posedge i_clk) begin
-        if (count == 19'b1000001111010110000) begin
-            count <= 19'b0;
+        if (count == 17'b10000011110101100) begin
+            count <= 17'b0;
             o_clockForDisplay <= ~o_clockForDisplay;
         end else begin
             count <= count + 1;
